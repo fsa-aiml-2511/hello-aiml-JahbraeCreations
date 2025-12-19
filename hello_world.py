4c7fad2e7cf63a0064c82f221a65340ccb65c4df
Jahbrae Creations <jahbrae@Jahbraes-MacBook-Pro.local>
2025-12-18 21:33:22 -0600

    changed name and comment

--- hello_world.py
+++ hello_world.py
@@ -1,10 +1,10 @@
 # Hello AI/ML Bootcamp!
 # Edit the variables below to introduce yourself
 
-name = "Your Name Here"
-fun_fact = "One fun fact about me"
+name = "Anthony"
+fun_fact = "I am a songwriter"
 
 # Don't change anything below this line
 print(f"Hello! I'm {name}.")
 print(f"Fun fact: {fun_fact}")
-print("I'm excited to learn AI/ML!")
+print("I am really that guy!")
